ab88187abf74922aea783638898a1afdc3f6c821
Charleira <cadupiacente67@gmail.com>
2023-05-31 20:07:42 -0300

    mudanças no codigo para receber os jogos

--- record.py
+++ record.py
@@ -1,14 +1,13 @@
 import requests
 import json
-import pytz
 
 API_KEY = "0daf50d6c4caa5f7ae3513bc52162612a78fd43fe65d6ea8842bb8cd8a26e914"
 BASE_URL = "https://apiv3.apifootball.com"
-print("funciona")
+
 def getPaisesDisponiveis():
     responseGetCountries = requests.get(f'{BASE_URL}/?action=get_countries&APIkey={API_KEY}')
     ListaFiltradaSemLogoPais = []
-    print(responseGetCountries)
+
     for country in responseGetCountries.json():
         country.pop('country_logo', None)
         ListaFiltradaSemLogoPais.append(country)
@@ -36,14 +35,22 @@ def getTabela(league_id):
     responseGetTabelaPretty = json.dumps(responseGetTabela.json(), indent=4)
     return responseGetTabelaPretty
 
-def getJogos(league_id):
-    tz = pytz.timezone('America/Sao_Paulo')
-    responsegetJogos = requests.get(f'{BASE_URL}?action=get_events&from=2023-07-12&to=2023-12-31&league_id={league_id}&APIkey={API_KEY}')
+def getJogos():
+    responsegetJogos = requests.get(f'{BASE_URL}?action=get_events&timezone=+07:00&APIkey={API_KEY}')
     responsegetJogosPretty = json.dumps(responsegetJogos.json(), indent=5)
-    print(responsegetJogos)
     return responsegetJogosPretty
 
+def getStatus():
+    responsegetStatus = requests.get(f'{BASE_URL}?action=get_statistics&match_id=86392&APIkye={API_KEY}')
+    responsegetStatusPretty = json.dumps(responsegetStatus.json(), indent=6)
+    return responsegetStatusPretty
+
+def getH2H():
+    responsegetH2H = requests.get(f'{BASE_URL}?action=get_H2H&firstTeamId=7275&secondTeamId=151&APIkey={API_KEY}')
+    responsegetH2HPretty = json.dumps(responsegetH2H.json(), indent=5)
+    responsegetH2HPretty
+    
 #print(getLigasDisponiveis())
 #print(getTimes(153))
 #print(getTabela(153))
-print(getJogos(153))
+#print(getJogos(153))
