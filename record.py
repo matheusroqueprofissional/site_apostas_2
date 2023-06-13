ca44abcc8bc6fb64dc75b3f4e9c2f709455421ea
matheusroqueprofissional <matheus.roqueprofissional@gmail.com>
2023-06-13 05:23:28 -0300

    *

--- record.py
+++ record.py
@@ -1,10 +1,11 @@
 import requests
 import json
 import pytz
+from datetime import datetime
 
 API_KEY = "0daf50d6c4caa5f7ae3513bc52162612a78fd43fe65d6ea8842bb8cd8a26e914"
 BASE_URL = "https://apiv3.apifootball.com"
-print("funciona")
+#print("funciona")
 def getPaisesDisponiveis():
     responseGetCountries = requests.get(f'{BASE_URL}/?action=get_countries&APIkey={API_KEY}')
     ListaFiltradaSemLogoPais = []
@@ -34,16 +35,40 @@ def getTimes(league_id):
 def getTabela(league_id):
     responseGetTabela = requests.get(f'{BASE_URL}/?action=get_standings&league_id={league_id}&APIkey={API_KEY}')
     responseGetTabelaPretty = json.dumps(responseGetTabela.json(), indent=4)
+    #responseGetTabelaPretty.country_name
     return responseGetTabelaPretty
 
 def getJogos(league_id):
-    tz = pytz.timezone('America/Sao_Paulo')
-    responsegetJogos = requests.get(f'{BASE_URL}?action=get_events&from=2023-07-12&to=2023-12-31&league_id={league_id}&APIkey={API_KEY}')
+    #tz = pytz.timezone('Europe/Berlin')
+    responsegetJogos = requests.get(f'{BASE_URL}?action=get_events&from=2023-02-12&to=2023-12-31&league_id={league_id}&APIkey={API_KEY}')
     responsegetJogosPretty = json.dumps(responsegetJogos.json(), indent=5)
     print(responsegetJogos)
     return responsegetJogosPretty
 
-#print(getLigasDisponiveis())
-#print(getTimes(153))
-#print(getTabela(153))
-print(getJogos(153))
+#def getJogosPassados(league_id):
+#    tz = pytz.timezone('Europe/London')
+#    responsegetJogos = requests.get(f'{BASE_URL}?action=get_events&from=2023-07-12&to=2023-12-31&league_id={league_id}&APIkey={API_KEY}')
+#    jogos = []
+#    
+#    jogos = responsegetJogos.json()
+#    print("Erro na resposta da API. Retornando lista vazia.")
+#
+#    jogos_passados = []
+#    for jogo in jogos:
+#        jogos = json.loads(responsegetJogos.text)
+#
+#        data_jogo = datetime.strptime(jogo['match_date'], '%Y-%m-%d %H:%M:%S').astimezone(tz)
+#        if data_jogo < datetime.now(tz):
+#            jogos_passados.append(jogo)
+#
+#    return jogos_passados
+#
+#
+#print(getJogosPassados(0))
+
+
+#print(getPaisesDisponiveis())
+#print(getLigasDisponiveis(0))
+#print(getTimes(149))
+#print(getTabela(149))
+print(getJogos(149))
